a4fbcd1ec257f08642e84ff39438a7738571124e
jaggerCoder <jagger.wang@outlook.com>
2017-06-17 20:58:00 +0800

    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,9 +15,8 @@ namespace netmvc.Controllers
 
         public IActionResult About()
         {
-            
-            ViewData["Message"] = "Your application description page.";
 
+            ViewData["Message"] = "Your application description page.";
             return View();
         }
 
